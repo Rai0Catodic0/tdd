66e014036221a3c856322aaa1864687960df3e98
Gabriel Lima Luz <gabriel.lima.luz@hotmail.com>
2021-02-24 14:11:06 -0300

    Movetd Fts into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,39 +1,9 @@
-# teste funcional == teste de aceitação == end to end test
+from .base import FunctionalTest
+from  selenium import  webdriver
+from  selenium.webdriver.common.keys import Keys
 
-import os
-from django.contrib.staticfiles.testing import  StaticLiveServerTestCase
-from unittest import skip
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from  selenium.common.exceptions import WebDriverException
-import time
 
-MAX_WAIT = 10
-
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://'+staging_server
-    
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self,row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise  e
-                time.sleep(0.5)
-
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -70,7 +40,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         # Satisfied, she goes back to sleep
-        
+    
     def test_mutiple_users_can_start_lists_at_different_urls(self):
         #edit starts a new to-do list 
         self.browser.get(self.live_server_url)
@@ -112,44 +82,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
     
         # Satisfied, they both go back to sleep
-
-    def test_layout_and_styling(self):
-        #Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # She mptcoes the input box nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x']+ inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-        # She starts a new list an sees the input nicely
-        # centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-    @skip
-    def test_cannot_add_empty_list_items(self):
-        # Edith goes to the home page and accidentally tries to submit
-        # an empty list item. She hits Enter on the empty input box
-
-        # The home page refreshes, and there is an error message saying
-        # that list items cannot be blank
-
-        # She tries again with some text for the item, which now works
-
-        # Perversely, she now decides to submit a second blank list item
-
-        # She receives a similar warning on the list page
-
-        # And she can correct it by filling some text in
-        self.fail('write me!')
